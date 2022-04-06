ee097c7c247f645a67785dca89f24e8b0ecf2626
RohitBachche <98477376+RohitBachche@users.noreply.github.com>
2022-04-06 10:47:11 +0530

    Add files via upload

---
+++ Project Code Blood bank/BackEndcode/src/main/java/com/example/demo/uRepo/BloodBankRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.uRepo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BloodBankRepository extends JpaRepository<BloodBank, Integer> {
+	
+
+}
