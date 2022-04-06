0b24b0d677801e4fb7990b9df40afa5368b54b08
RohitBachche <98477376+RohitBachche@users.noreply.github.com>
2022-04-06 14:59:49 +0530

    Delete BloodBankRepository.java

--- Project Code Blood bank/BackEndcode/src/main/java/com/example/demo/uRepo/BloodBankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.uRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BloodBankRepository extends JpaRepository<BloodBank, Integer> {
-	
-
-}
