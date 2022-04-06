505a229c87a9cc65c34388b56c0e9422cc5a4035
RohitBachche <98477376+RohitBachche@users.noreply.github.com>
2022-04-06 10:46:28 +0530

    Add files via upload

---
+++ Project Code Blood bank/BackEndcode/src/main/java/com/example/demo/UService/BloodBankService.java
@@ -0,0 +1,12 @@
+package com.example.demo.UService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.uRepo.UserRepositroy;
+
+public class BloodBankService {
+
+	@Autowired
+	BloodBank repo;
+	
+}
